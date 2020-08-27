fa21eccb6e861e896c4a585840e46e0e933b6e20
stephdraggo <stephdraggo@users.noreply.github.com>
2020-08-27 15:53:43 +1000

    heehee

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [AddComponentMenu("Runner/Player Control")]
+    public class PlayerControl : MonoBehaviour
+    {
+        #region Variables
+        [Header("Reference Variables")]
+        public Rigidbody2D rigidBody;
+        #endregion
+        void Start()
+        {
+            rigidBody = GetComponent<Rigidbody2D>(); //connect rigidbody
+
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+/*
+ public Animator animator;
+
+ 
+
+    private void Start()
+    {
+            
+    }
+    void Update()
+    {
+        //make sure the "isRunning" is the same name as the bool in the animator
+        animator.SetBool("isRunning", false); 
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            //if isRunning true then change to running animation 
+            Debug.Log("isRunning");
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            //else isRunning false then change to idle animation 
+            animator.SetBool("isRunning", false);
+        }
+    }
+ */
